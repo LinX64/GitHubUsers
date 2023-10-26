958fe5cbc0fe7afd07d55515d1af5035c2a137b8
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-10-26 17:55:55 +0200

    Increasing the per page number items

--- app/src/main/java/com/client/githubusers/data/UsersApi.kt
+++ app/src/main/java/com/client/githubusers/data/UsersApi.kt
@@ -4,11 +4,14 @@ import com.client.githubusers.data.model.UserDetailResponse
 import com.client.githubusers.data.model.UserItem
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface UsersApi {
 
     @GET("users")
-    suspend fun getUsers(): List<UserItem>
+    suspend fun getUsers(
+        @Query("per_page") perPage: Int = 100
+    ): List<UserItem>
 
     @GET("users/{name}")
     suspend fun getUserDetails(@Path("name") name: String): UserDetailResponse
