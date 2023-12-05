1e4906050628f1601430082a4aa344a166af310d
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-12-05 16:37:44 +0100

    Added the injected dispatcher

--- app/src/main/java/com/client/githubusers/data/repository/UsersRepositoryImpl.kt
+++ app/src/main/java/com/client/githubusers/data/repository/UsersRepositoryImpl.kt
@@ -3,22 +3,24 @@ package com.client.githubusers.data.repository
 import com.client.githubusers.data.UsersApi
 import com.client.githubusers.data.model.UserDetailResponse
 import com.client.githubusers.data.model.UserItem
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 class UsersRepositoryImpl(
-    private val usersApi: UsersApi
+    private val usersApi: UsersApi,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : UsersRepository {
 
     override fun getUsers(): Flow<List<UserItem>> = flow {
         val users = usersApi.getUsers()
         emit(users)
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(ioDispatcher)
 
     override fun getUserDetailsByName(name: String): Flow<UserDetailResponse> = flow {
         val user = usersApi.getUserDetails(name)
         emit(user)
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(ioDispatcher)
 }
--- app/src/main/java/com/client/githubusers/di/AppModule.kt
+++ app/src/main/java/com/client/githubusers/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.client.githubusers.di
 import com.client.githubusers.domain.UserSearchUseCase
 import com.client.githubusers.ui.views.userDetail.UserDetailViewModel
 import com.client.githubusers.ui.views.users.UserSearchViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -11,5 +13,6 @@ val appModule = module {
     viewModelOf(::UserSearchViewModel)
     viewModelOf(::UserDetailViewModel)
 
+    single<CoroutineDispatcher> { Dispatchers.IO }
     singleOf(::UserSearchUseCase)
 }
